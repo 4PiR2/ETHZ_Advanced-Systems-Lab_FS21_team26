fc8e32a426139942e5870d2f5cb06ba301bc4a50
shejin <shejin@student.ethz.ch>
2021-05-28 09:42:15 +0000

    Upload New File

---
+++ code/src/GD/GD_scalar and blocking/include/baselines.h
@@ -0,0 +1,12 @@
+#ifndef BASELINES_H
+#define BASELINES_H
+
+#include <stdlib.h>
+#include "mat.h"
+
+void _getPairwiseAffinity(const float* ED, int n_samples, float perp, float* P);
+void _normalizeData(float* X, int n_samples, int d_in);
+void _symmetrizeAffinities(float* P, int n_samples);
+void _getSquaredEuclideanDistances(const float* X, int n_samples, int dim, float* DD);
+void baselineCompare(const float* X, const float* Y, int size);
+#endif
