00e42c4dadd0f2d4de7c1e5e797588d4308f58b4
jinsz <1257812078@qq.com>
2021-06-06 00:12:07 +0200

    GD new

---
+++ code/src/GD_avx2/interface.h
@@ -0,0 +1,63 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#define ALIGN_ELEM 1
+
+#include "../common/mat.h"
+#include "pre.h"
+#include "gd.h"
+
+void readData(float *x, float *y, const std::string &filename, int seed, int n_samples, int d_in, int d_out) {
+	mat_rand_norm(y, n_samples, d_out, 0.f, 1e-4f, true, seed);
+	mat_load(x, n_samples, d_in, filename);
+}
+
+void getSymmetricAffinity(float *x, int n_samples, int d_in, float perplexity, float *p, float *d) {
+	_getSquaredEuclideanDistances(x, n_samples, d_in, d);
+	_getPairwiseAffinity(d, n_samples, perplexity, p);
+	_symmetrizeAffinities(p, n_samples);
+}
+
+void getLowDimResult(float *y, float *u, float *g, float *p, float *t, int n_samples, int d_out, float alpha,
+                     float eta, int n_iter) {
+	for (int i = 0; i < n_iter; i++) {
+		float t_sum_inv = compute_t(y, t, n_samples, d_out);
+		gradientCompute(y, g, p, t, t_sum_inv, n_samples, d_out);
+		gradientUpdate(y, u, g, n_samples, d_out, alpha, eta);
+	}
+}
+
+static void
+run(int n_samples, int d_out, int d_in, int rep, float eta, float alpha, float perplexity, int n_iter,
+    const std::string &file_in, const std::string &file_out) {
+	thandle t1 = create_timer("SymAff"), t2 = create_timer("GradDesc");
+	for (int r = 0; r < rep; r++) {
+		printf("Rep %d starting...\n", r + 1);
+		auto x = mat_alloc<float>(n_samples, d_in),
+				p = mat_alloc<float>(n_samples, n_samples),
+				t = mat_alloc<float>(n_samples, n_samples),
+				y = mat_alloc<float>(n_samples, d_out),
+				u = mat_alloc<float>(n_samples, d_out),
+				g = mat_alloc<float>(n_samples, d_out),
+				d = mat_alloc<float>(n_samples, n_samples);
+		readData(x, y, file_in, 13, n_samples, d_in, d_out);
+
+		start(t1);
+		getSymmetricAffinity(x, n_samples, d_in, (float) perplexity, p, d);
+		stop(t1);
+
+		mat_store(p, n_samples, n_samples, "../output/p_matrix.txt");
+
+		start(t2);
+		getLowDimResult(y, u, g, p, t, n_samples, d_out, alpha, eta, n_iter);
+		stop(t2);
+
+		mat_store(y, n_samples, d_out, file_out);
+
+		mat_free_batch(7, x, p, t, y, u, g, d);
+	}
+
+	benchmark_print();
+}
+
+#endif //INTERFACE_H
---
+++ code/src/GD_avx2/pre.h
@@ -0,0 +1,97 @@
+#ifndef PRE_H
+#define PRE_H
+
+#include <cmath>
+
+// define the maximum number of iterations to fit the perplexity
+int MAX_ITERATIONS = 200;
+
+// define the error tolerance for the perplexity
+float ERROR_TOLERANCE = 1e-5f;
+
+void _getSquaredEuclideanDistances(float *x, int n_samples, int d_in, float *d) {
+	for (int i = 0; i < n_samples; i++) {
+		d[i * n_samples + i] = 0.f;
+		for (int j = i + 1; j < n_samples; j++) {
+			float dist = x[i * d_in] - x[j * d_in];
+			dist *= dist;
+			for (int k = 1; k < d_in; k++) {
+				float diff = x[i * d_in + k] - x[j * d_in + k];
+				dist += diff * diff;
+			}
+			d[j * n_samples + i] = d[i * n_samples + j] = dist;
+		}
+	}
+}
+
+void _getPairwiseAffinity(float *d, int n_samples, float perplexity, float *p) {
+	float log_perp = logf(perplexity), lb = log_perp - ERROR_TOLERANCE, rb = log_perp + ERROR_TOLERANCE;
+	// compute affinities row by row
+	for (int i = 0; i < n_samples; i++) {
+		float maxv = std::numeric_limits<float>::min();
+		for (int j = 0; j < n_samples; j++) {
+			float dist = d[i * n_samples + j];
+			if (dist > maxv) {
+				maxv = dist;
+			}
+		}
+		// initialize beta values, beta := -.5f / (sigma * sigma)
+		float beta = -1.f / maxv, beta_max, beta_min, sum;
+		bool flag0 = true, flag1 = true;
+		// perform binary search to find the optimal beta values for each data point
+		for (int k = 0; k < MAX_ITERATIONS; ++k) {
+			// compute the conditional Gaussian densities for point i
+			sum = 0.f;
+			float shannon_entropy = 0.f;
+			for (int j = 0; j < n_samples; j++) {
+				if (i != j) {
+					float bd = beta * d[i * n_samples + j];
+					float gaussian_density = expf(bd);
+					shannon_entropy -= bd * gaussian_density;
+					sum += gaussian_density;
+					p[i * n_samples + j] = gaussian_density;
+				}
+			}
+			shannon_entropy = shannon_entropy / sum + logf(sum);
+			if (shannon_entropy > lb && shannon_entropy < rb) {
+				break;
+			} else {
+				if (shannon_entropy > log_perp) {
+					beta_min = beta;
+					flag1 = false;
+					if (flag0)
+						beta *= 2.f;
+					else
+						beta = (beta + beta_max) * .5f;
+				} else {
+					beta_max = beta;
+					flag0 = false;
+					if (flag1)
+						beta *= .5f;
+					else
+						beta = (beta + beta_min) * .5f;
+				}
+			}
+		}
+		float sum_inv = 1.f / sum;
+		// normalize the row
+		for (int j = 0; j < n_samples; j++) {
+			if (i != j) {
+				p[i * n_samples + j] *= sum_inv;
+			}
+		}
+		p[i * n_samples + i] = 0.f;
+	}
+}
+
+void _symmetrizeAffinities(float *p, int n_samples) {
+	auto p_sum_inv = .5f / float(n_samples);
+	for (int i = 0; i < n_samples; i++) {
+		for (int j = i + 1; j < n_samples; j++) {
+			float p_ij = p[i * n_samples + j], p_ji = p[j * n_samples + i];
+			p[j * n_samples + i] = p[i * n_samples + j] = (p_ij + p_ji) * p_sum_inv;
+		}
+	}
+}
+
+#endif //PRE_H
