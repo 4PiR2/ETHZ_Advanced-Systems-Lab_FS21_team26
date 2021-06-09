7fe20b231b72f5089e5cc155e2fb338772f1c18b
Levin Moser <levin.moser@outlook.com>
2021-06-09 17:05:31 +0200

    [OPT] use expf approximation with scalar_up1

--- code/src/scalar/sym_aff.h
+++ code/src/scalar/sym_aff.h
@@ -131,10 +131,17 @@ inline void avx2_dp_sum(float* x, float* y, float* ret_dp, float* ret_sum, int s
 
 // deleting the exp functions
 //#define SYM_AFF_PA_SCALAR_INIT // 1.83, 2.08
+
 // deleting one branch
 // #define SYM_AFF_PA_SCALAR_UP1 // 2.33 2.62
+
 // deleting beta values
 // #define SYM_AFF_PA_SCALAR_UP2 // 2.11
+// abandoned -> no significant speedup
+
+// use exponential function approximation, based on SCALAR_UP1
+// #define SYM_AFF_PA_SCALAR_UP3
+
 // try avx2 functions
 // #define SYM_AFF_PA_AVX2 // 7.0(no-vec flag) 1.8(normal)
 // #define SYM_AFF_PA_SCALAR_CURRENT 
@@ -325,6 +332,7 @@ void getPairwiseAffinity(float *d, int n_samples, float perplexity, float *p) {
 
 #endif // SYM_AFF_SCALAR_UP1
 
+
 #ifdef SYM_AFF_PA_SCALAR_UP2
 // will modify euclidean distance file
 void getPairwiseAffinity(float *d, int n_samples, float perplexity, float *p) {
@@ -423,24 +431,17 @@ void getPairwiseAffinity(float *d, int n_samples, float perplexity, float *p) {
 }
 #endif // SYM_AFF_SCALAR_UP2
 
-// Replace the 'expf' function call by an approximation
-// Improve stopping criteria
+
+// Replace the 'expf' function call by an approximation, based on up1
 #ifdef SYM_AFF_PA_SCALAR_UP3
 
 // Approximation of the exponential function by linear regression (polynomial of order three)
 inline float exp_app(float x) {
 	// -1 <= x <= 0
 	float y;
-	// a0 = _mm512_set1_ps(.9996136409397813f),
-	// a1 = _mm512_set1_ps(.9920487460431511f),
-	// a2 = _mm512_set1_ps(.4624692123106021f),
-	// a3 = _mm512_set1_ps(.10250045262707179f);
 
-	//y = _mm512_fmadd_ps(a3, x, a2);
 	y = (.10250045262707179f * x) + .4624692123106021f;
-	//y = _mm512_fmadd_ps(y, x, a1);
 	y = (y * x) + .9920487460431511f;
-	//y = _mm512_fmadd_ps(y, x, a0);
 	y = (y * x) + .9996136409397813f;
 
 	return y;
@@ -453,23 +454,22 @@ void getPairwiseAffinity(float *d, int n_samples, float perplexity, float *p) {
 	float* bd_exps = (float*)malloc(n_samples * sizeof(float));
 	float* bd_max_exps = (float*)malloc(n_samples * sizeof(float));
 	float* bd_min_exps = (float*)malloc(n_samples * sizeof(float));
-	float* bds = (float*)malloc(n_samples * sizeof(float));
-	float* bd_maxs = (float*)malloc(n_samples * sizeof(float));
-	float* bd_mins = (float*)malloc(n_samples * sizeof(float));
 
+	// compute affinities row by row
 	for (int i = 0; i < n_samples; i++) {
-		int row = i * n_samples;
 		float maxv = std::numeric_limits<float>::min();
 		for (int j = 0; j < n_samples; j++) {
-			float dist = d[row + j];
+			float dist = d[i * n_samples + j];
 			if (dist > maxv) {
 				maxv = dist;
 			}
 		}
-		float beta = -1.f / maxv, sum;
+		// initialize beta values, beta := -.5f / (sigma * sigma)
+		float beta = -1.f / maxv, beta_max, beta_min, sum;
+		// change all betas to exps
 		for (int j = 0; j < n_samples; j++) {
-			bds[j] = beta * d[row + j];
-			bd_exps[j] = exp_app(bds[j]);
+			float dist = d[i * n_samples + j];
+			bd_exps[j] = exp_app(beta * dist);
 		}
 		bool flag0 = true, flag1 = true;
 
@@ -477,69 +477,69 @@ void getPairwiseAffinity(float *d, int n_samples, float perplexity, float *p) {
 		for (int k = 0; k < MAX_ITERATIONS; ++k) {
 			// compute the conditional Gaussian densities for point i
 			sum = 0.f;
-			bds[i] = bd_exps[i] = 0.f;
+			bd_exps[i] = 0.f;
 			float shannon_entropy = 0.f;
 			for (int j = 0; j < n_samples; j++) {
-				float bd = bds[j];
+				float bd = beta * d[i * n_samples + j];
 				float gaussian_density = bd_exps[j];
 				shannon_entropy -= bd * gaussian_density;
 				sum += gaussian_density;
+				p[i * n_samples + j] = gaussian_density;
 			}
 			shannon_entropy = shannon_entropy / sum + logf(sum);
 			if (shannon_entropy > lb && shannon_entropy < rb) {
 				break;
-			}
-			if (shannon_entropy > log_perp) {
-				swapptr(&bd_exps, &bd_min_exps);
-				swapptr(&bds, &bd_mins);
-			}
-			else {
-				swapptr(&bd_exps, &bd_max_exps);
-				swapptr(&bds, &bd_maxs);
-			}
-			if (flag0 || flag1) {
+			} else {
 				if (shannon_entropy > log_perp) {
+					beta_min = beta;
+					float* tmp = bd_exps;
+					bd_exps = bd_min_exps;
+					bd_min_exps = tmp;
+
 					flag1 = false;
 					if (flag0) {
-						for (int j = 0; j < n_samples; j++) {
-							bd_exps[j] = bd_min_exps[j] * bd_min_exps[j];
-							bds[j] = 2.f * bd_mins[j];		
-						}
+						beta *= 2.f;
+						for (int j = 0; j < n_samples; j++)
+							bd_exps[j] = tmp[j] * tmp[j];
 					}
 					else {
-						for (int j = 0; j < n_samples; j++) {
-							bd_exps[j] = sqrtf(bd_min_exps[j] * bd_max_exps[j]);
-							bds[j] = (bd_mins[j] + bd_maxs[j]) * .5f;
-						}
+						beta = (beta + beta_max) * .5f;
+						for (int j = 0; j < n_samples; j++)
+							bd_exps[j] = sqrtf(tmp[j] * bd_max_exps[j]);
 					}
 				} else {
+					beta_max = beta;
+					float* tmp = bd_exps;
+					bd_exps = bd_max_exps;
+					bd_max_exps = tmp;
+
 					flag0 = false;
 					if (flag1) {
-						for (int j = 0; j < n_samples; j++) {
-							bd_exps[j] = sqrtf(bd_max_exps[j]);
-							bds[j] = .5f * bd_maxs[j];
-						}
+						beta *= .5f;
+						for (int j = 0; j < n_samples; j++)
+							bd_exps[j] = sqrtf(tmp[j]);
 					}
 					else {
-						for (int j = 0; j < n_samples; j++) {
-							bd_exps[j] = sqrtf(bd_max_exps[j] * bd_min_exps[j]);
-							bds[j] = (bd_mins[j] + bd_maxs[j]) * .5f;
-						}
+						beta = (beta + beta_min) * .5f;
+						for (int j = 0; j < n_samples; j++)
+							bd_exps[j] = sqrtf(tmp[j] * bd_min_exps[j]);
 					}
 				}
 			}
-			else {
-				for (int j = 0; j < n_samples; j++) {
-					bd_exps[j] = sqrtf(bd_max_exps[j] * bd_min_exps[j]);
-					bds[j] = (bd_mins[j] + bd_maxs[j]) * .5f;
-				}
-			}
-			
 		}
+		float sum_inv = 1.f / sum;
+		// normalize the row
+		for (int j = 0; j < n_samples; j++) {
+			if (i != j) {
+				p[i * n_samples + j] *= sum_inv;
+			}
+		}
+		p[i * n_samples + i] = 0.f;
 	}
 
-	free(bd_exps), free(bd_max_exps), free(bd_min_exps), free(bds), free(bd_maxs), free(bd_mins);
+	free(bd_exps), free(bd_max_exps), free(bd_min_exps);
 }
+
 #endif // SYM_AFF_SCALAR_UP3
 
 #ifdef SYM_AFF_PA_AVX2
